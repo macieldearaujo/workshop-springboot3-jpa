507f3fa33c7d9aa3b81ca1664219771c7d3052a5
douglasmcl <douglasmacielaraujo@outlook.com>
2024-04-19 01:34:53 -0300

    implement testConfig

--- course/src/main/java/com/educandoweb/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/config/TestConfig.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.repository.UserRepository;
 
 @Configuration
-@Profile("test")
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
@@ -19,12 +17,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		User u1 = new User(null, "Douglas Maciel", "doug@gmail.com", "99999999", "12345");
-		User u2 = new User(null, "Isabelly Morais", "isinha@gmail.com", "97777777", "12345");
-		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		
 	}
 }
